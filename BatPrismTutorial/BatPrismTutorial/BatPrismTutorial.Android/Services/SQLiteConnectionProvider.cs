1ced116c87a0b8ea5a2d8d0be20cfcb27e1ba662
Josue Romero <rj.josueromero@gmail.com>
2019-07-08 15:26:27 -0600

    La implementacion en Android ahora es asincrona

--- BatPrismTutorial/BatPrismTutorial/BatPrismTutorial.Android/Services/SQLiteConnectionProvider.cs
+++ BatPrismTutorial/BatPrismTutorial/BatPrismTutorial.Android/Services/SQLiteConnectionProvider.cs
@@ -18,15 +18,35 @@ namespace BatPrismTutorial.Droid.Services
 {
     class SQLiteConnectionProvider: ISQLiteConnectionProvider
     {
-        private SQLiteConnection Connection { get; set; }
+        //private SQLiteConnection Connection { get; set; }
+        private SQLiteAsyncConnection Connection { get; set; }
 
-        public SQLiteConnection GetConnection()
+        public SQLiteAsyncConnection GetConnection()
         {
             if (this.Connection != null) { return this.Connection; }
 
             var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
             path = Path.Combine(path, "database.db3");
-            return this.Connection = new SQLiteConnection(path);
+            return this.Connection = new SQLiteAsyncConnection(path);
+        }
+
+        /* public SQLiteConnection GetConnection()
+         {
+             if (this.Connection != null) { return this.Connection; }
+
+             var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+             path = Path.Combine(path, "database.db3");
+             return this.Connection = new SQLiteConnection(path);
+         }*/
+
+
+        public SQLiteAsyncConnection GetConnection()
+        {
+            if (this.Connection != null) { return this.Connection; }
+
+            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            path = Path.Combine(path, "database.db3");
+            return this.Connection = new SQLiteAsyncConnection(path);
         }
     }
 }
