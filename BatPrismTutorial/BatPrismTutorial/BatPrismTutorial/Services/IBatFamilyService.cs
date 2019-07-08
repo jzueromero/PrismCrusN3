bb88dfa2d0e887afe9c956b05258faceab85b9f9
Josue Romero <rj.josueromero@gmail.com>
2019-07-08 15:34:21 -0600

    Ahora la interfaz busca implementaciones como tareas

--- BatPrismTutorial/BatPrismTutorial/BatPrismTutorial/Services/IBatFamilyService.cs
+++ BatPrismTutorial/BatPrismTutorial/BatPrismTutorial/Services/IBatFamilyService.cs
@@ -2,15 +2,22 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BatPrismTutorial.Services
 {
     public interface IBatFamilyService
     {
-        IEnumerable<BatFamily> GetAll();
-        BatFamily GetById(int id);
-        void Update(BatFamily BatParent);
-        void Insert(BatFamily BatParent);
-        void Delete(int id);
+        /* IEnumerable<BatFamily> GetAll();
+         BatFamily GetById(int id);
+         void Update(BatFamily BatParent);
+         void Insert(BatFamily BatParent);
+         void Delete(int id);*/
+
+        Task<IEnumerable<BatFamily>> GetAllAsync();
+        Task<BatFamily> GetByIdAsync(int id);
+        Task UpdateAsync(BatFamily todoItem);
+        Task InsertAsync(BatFamily todoItem);
+        Task DeleteAsync(BatFamily todoItem);
     }
 }
