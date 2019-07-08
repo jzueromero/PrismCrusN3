b1f00e43df8370793dece26c00b538755024f70e
Josue Romero <rj.josueromero@gmail.com>
2019-07-08 15:22:44 -0600

    Ahora se implementara ConexionAsincrona

--- BatPrismTutorial/BatPrismTutorial/BatPrismTutorial/Services/ISQLiteConnectionProvider.cs
+++ BatPrismTutorial/BatPrismTutorial/BatPrismTutorial/Services/ISQLiteConnectionProvider.cs
@@ -7,6 +7,7 @@ namespace BatPrismTutorial.Services
     public interface ISQLiteConnectionProvider
     {
         //SQLiteConnection GetConnection();
+        //Ahora usaremos conexion asincrona
         SQLiteAsyncConnection GetConnection();
     }
 }
